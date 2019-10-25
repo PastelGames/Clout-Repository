b186186f2ac8564dd0a6e2263d96e6ff063ba205
El Tucker <elchristopher@outlook.com>
2019-10-25 03:31:44 -0400

    Initial Commit

---
+++ Clout/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Gameplay : MonoBehaviour
+{
+    public int followers;
+    public TMP_Text followerCountText;
+    public Canvas FPSCanvas;
+    public Canvas PhoneCanvas;
+    public GameObject FPSController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowerCountTextUpdate();
+    }
+
+    void FollowerCountTextUpdate()
+    {
+        followerCountText.SetText("Bodies Sacrificed: " + followers);
+    }
+
+    public void SwitchCanvases()
+    {
+        FPSCanvas.gameObject.SetActive(!FPSCanvas.gameObject.activeSelf);
+        PhoneCanvas.gameObject.SetActive(!PhoneCanvas.gameObject.activeSelf);
+        FPSController.gameObject.GetComponent<FirstPersonController>().enabled =
+            !FPSController.gameObject.GetComponent<FirstPersonController>().enabled;
+        Cursor.visible = !Cursor.visible;
+        if (Cursor.lockState == CursorLockMode.Locked) Cursor.lockState = CursorLockMode.None;
+        else Cursor.lockState = CursorLockMode.Locked;
+    }
+
+}
+
---
+++ Clout/Assets/Scripts/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public bool follower;
+    GameObject usernameCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        usernameCanvas = gameObject.transform.Find("Username Canvas").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayText()
+    {
+        if (follower == true)
+        {
+            usernameCanvas.SetActive(true);
+        }
+    }
+}
