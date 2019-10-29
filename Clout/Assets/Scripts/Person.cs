7b27515bce44e4a64fa1d5b7223fda9ee737cdd2
El Tucker <elchristopher@outlook.com>
2019-10-28 23:12:41 -0400

    Added follower movement pattern

--- Clout/Assets/Scripts/Person.cs
+++ Clout/Assets/Scripts/Person.cs
@@ -6,24 +6,65 @@ public class Person : MonoBehaviour
 {
     public bool follower;
     GameObject usernameCanvas;
+    public float moveSpeed;
+    static float maxX = 4.5f;
+    static float maxZ = 4.5f;
+    static float minX = -4.5f;
+    static float minZ = -4.5f;
+    Vector3 targetPosition;
+    public bool rotating;
+    public float rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         usernameCanvas = gameObject.transform.Find("Username Canvas").gameObject;
+        targetPosition = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void DisplayText()
-    {
-        if (follower == true)
+        if (!follower)
+        {
+            PersonWander();
+        }
+        else if (!usernameCanvas.activeSelf)
         {
             usernameCanvas.SetActive(true);
         }
     }
+
+    public void DisplayText()
+    {
+       
+    }
+
+    void PersonWander()
+    {
+        if (!rotating)
+        {
+            float maxSpeed = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, maxSpeed);
+            transform.LookAt(targetPosition);
+        }
+        else
+        {
+            Vector3 targetDir = targetPosition - transform.position;
+            float maxRadians = rotateSpeed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotateSpeed, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDir);
+            if (Vector3.Angle(targetDir, transform.forward) <= .1f)
+            {
+                rotating = false;
+            }
+        }
+        if (Vector3.Distance(targetPosition, transform.position) < .001f)
+        {
+            targetPosition = targetPosition = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+            rotating = true;
+        }
+    }
+
+
 }
---
+++ Clout/Assets/Scripts/PersonData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Person Data", menuName = "Person Data", order = 1)]
+public class PersonData : ScriptableObject
+{
+    public string username;
+    public Image[] pictures;
+    public Mesh personMesh;
+}
